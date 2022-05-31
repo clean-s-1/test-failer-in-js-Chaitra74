f34b254bcf8fa3f36d6f7a7e368ec1c7a9f324af
Chaitra74 <chai7495@gmail.com>
2022-05-31 19:04:38 +0530

    updated test cases

--- alerter.js
+++ alerter.js
@@ -1,3 +1,5 @@
+const {expect} = require("chai");
+
 let alertFailureCount = 0;
 
 function networkAlertStub(celcius) {
@@ -5,22 +7,28 @@ function networkAlertStub(celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    if(celcius > 200){
+        return 500;
+    }    
     return 200;
 }
 
-function alertInCelcius(farenheit) {
+function alertInCelcius(farenheit, networkStub) {
     const celcius = (farenheit - 32) * 5 / 9;
-    const returnCode = networkAlertStub(celcius);
+    const returnCode = networkStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-    }
+        alertFailureCount += 1;
+    }   
 }
 
-alertInCelcius(400.5);
-alertInCelcius(303.6);
+alertInCelcius(303.6, networkAlertStub);
+alertInCelcius(400.5, networkAlertStub);
+
+expect(alertFailureCount).equals(1);
+
 console.log(`${alertFailureCount} alerts failed.`);
 console.log('All is well (maybe!)');
--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,30 @@
 const {expect} = require('chai')
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+const colorsMap = new Map();
+const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+
+function calculate_color_map(){    
     for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+        for (let j = 0; j < minorColors.length; j++) {           
+            let pairIndex = i * 5 + j;
+            colorsMap.set(pairIndex, `| ${majorColors[i]} | ${minorColors[j]}`)
         }
     }
-    return majorColors.length * minorColors.length;
+    return colorsMap;
 }
 
-result = print_color_map();
-expect(result).equals(25);
+function print_color_map(colorMap) {
+    colorMap.forEach((element, key) => {       
+        console.log(`${key} ${element}`);        
+    });   
+}
+
+colormap = calculate_color_map();
+result = print_color_map(colormap);
+
+expect(colormap.size).equals(25);
+expect(colormap.get(0)).equals(undefined);
+expect(colormap.get(1)).equals('| White | Blue');
+
 console.log('All is well (maybe!)');
