96cc3a2d805dd5f16e1a608097b09a56d5222643
Chaitra74 <chai7495@gmail.com>
2022-05-31 19:05:23 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -21,7 +21,7 @@ function alertInCelcius(farenheit, networkStub) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 1;
+        alertFailureCount += 0;
     }   
 }
 
